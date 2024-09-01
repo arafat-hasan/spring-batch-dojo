0a03035f6f44d5bfd9c2d016d89ad44cf106b11b
faiyazsadi <sadifaiyaz@gmail.com>
2024-09-01 19:22:28 +0600

    add

--- src/main/java/com/example/learnspringbatch/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.learnspringbatch.repository;
-
-import com.example.learnspringbatch.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
